5d060f1fd3aa507633ec58c31a72d00315fda0a0
Leonor Machado Llansol <lml@s226-05.l2f.inesc-id.pt>
2020-03-02 14:27:13 +0000

    added agents folder

---
+++ agents/externalAgents/YesNoAgent/YesNoAgent.py
@@ -0,0 +1,25 @@
+import time,sys
+import random
+
+class YesNoAgent:
+    def __init__(self,configs,indexval=''):
+        self.agentName = self.__class__.__name__
+        self.questionSimValue = float(configs['questionSimValue'])
+        self.answerSimValue = float(configs['answerSimValue'])
+        self.normalizeUserInput = True
+
+
+
+    def requestAnswer(self,userInput,candidates):
+
+        possibleAnswers = ["Sim.", "Não.", "Talvez."]
+        
+        # TODO ter em conta o contexto
+        # TODO ver se há respostas nos candidates que contenham Sim, Não, Talvez 
+        finalAnswer = random.choice(possibleAnswers)
+
+        return finalAnswer
+
+
+    def getFinalScore(self,questionScore,answerScore):
+        return questionScore * self.questionSimValue + answerScore * self.answerSimValue
---
+++ agents/internalAgents/talkpedia/__init__.py
@@ -0,0 +1,3 @@
+import os
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+os.chdir(CURRENT_DIR)
