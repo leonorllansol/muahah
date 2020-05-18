5b5f0d1f4ff8ad2de1a08f7d92722d15101b9bce
Leonor Llansol <leonor.llansol@gmail.com>
2020-05-18 17:47:56 +0100

    remove old repo

--- agents/internalAgents/talkpedia/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-os.chdir(CURRENT_DIR)
-import agents.internalAgents.talkpedia
-import agents.internalAgents.talkpedia.talkpedia
--- dialog/ReferenceTA.py
+++
@@ -1,9 +0,0 @@
-class ReferenceTA:
-
-    def __init__(self, dialogId, trigger, answer):
-        self.dialogId = dialogId
-        self.trigger = trigger
-        self.answer = answer
-
-    def __repr__(self):
-        return "ReferenceTA{" + "dialogId=" + dialogId + ", trigger='" + trigger + '\'' + ", answer='" + answer + '\'' + '}'
